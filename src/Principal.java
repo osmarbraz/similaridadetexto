3fbf79e55a89457a2b5760d370b5d155e5a99088
91269423991 <91269423991@EADPROF118921.corp.udesc.br>
2018-09-06 17:23:06 -0300

    gram

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,5 @@
 
-import java.text.Normalizer;
 import java.util.List;
-import org.simmetrics.StringMetric;
-import static org.simmetrics.builders.StringDistanceBuilder.with;
-import org.simmetrics.metrics.Levenshtein;
-import org.simmetrics.metrics.StringMetrics;
 import org.simmetrics.simplifiers.Simplifier;
 import org.simmetrics.simplifiers.Simplifiers;
 import org.simmetrics.tokenizers.Tokenizer;
@@ -23,45 +18,31 @@ public class Principal {
         String ref2 ="H. Wang, et.al. Personalized ranking model adaptation for web search. In Proc. Intl. SIGIR; pg. 323 a 332, 20113.";
         
        
-        Simplifier simplificador1 = Simplifiers.replaceNonWord(" ");
-        
+        Simplifier simplificador1 = Simplifiers.replaceNonWord(" ");        
         ref1 = simplificador1.simplify(ref1);                        
-        System.out.println("resultado1:"+ref1);
+        System.out.println("simplificador1:"+ref1);
         
-        //Simplifier simplificador2 = Simplifiers.toLowerCase();      
-        //ref1 = simplificador2.simplify(ref1);                        
-        //System.out.println("resultado2:"+ref1);
+        Simplifier simplificador2 = Simplifiers.toLowerCase();      
+        ref1 = simplificador2.simplify(ref1);                        
+        System.out.println("simplificador2:"+ref1);
         
-        Tokenizer tokenizador = Tokenizers.whitespace();
-        List<String> refs1 = tokenizador.tokenizeToList(ref1);
+        Tokenizer tokenizador1 = Tokenizers.whitespace();
+        List<String> refs1 = tokenizador1.tokenizeToList(ref1);
         
         for(String a : refs1){
             System.out.println("token = " + a);
             if (a.length()>1){
                 int ngram = formula(a.length());
-                
+                 Tokenizer tokenizador2 = Tokenizers.qGram(ngram);
+                 List<String> refs2 = tokenizador2.tokenizeToList(a);
+                 System.out.print("Tokens =");
+                 for(String b : refs2){
+                     System.out.print("," + b);
+                 }
+                 
                 
             }
 	}
-        
-        
-        
-        
-        
-//        Simplifier simplificador3 = Simplifiers.normalize(Normalizer.Form.NFD);
-//        ref1 = simplificador3.simplify(ref1);                        
-//        System.out.println("resultado2:"+ref1);
-        
-        
-        //String[] palavrasRef1 = ref1.split(" ");
-        
-        //for(int i=0;i<palavrasRef1.length;i++){
-        //System.out.println("tokens:"+palavrasRef1[i] + " =  "+palavrasRef1[i].length());
-        //}
-        
-        
-        // StringMetric metric = StringMetrics.longestCommonSubsequence();
-        //ystem.out.println("metrica = " + metric.compare(ref1, ref2));
         
     }
     
